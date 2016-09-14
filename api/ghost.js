dc138feadd28cdf3663ae1948fbb7d7ee50c009b
Eric Caleb <calebme@mail.com>
2016-09-14 13:40:31 +0100

    Added content type and stringfy JSON data

--- api/ghost.js
+++ api/ghost.js
@@ -2,6 +2,8 @@ var http =  require("http");
 var data = require("./data/country");
 
 http.createServer(function(req, res){
+    res.writeHead(200 , {"Content-Type":"text/json"});
+    res.end(JSON.stringify(data));
 
 }).listen(4000);
 
