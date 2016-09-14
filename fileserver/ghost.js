d07f19a486712df86bf1c6b6a74ee27d720864d4
Eric Caleb <calebme@mail.com>
2016-09-14 13:45:16 +0100

    Mapping root path for file server / Serve HTML ContentType

--- fileserver/ghost.js
+++ fileserver/ghost.js
@@ -5,5 +5,11 @@ var path = require("path");
 http.createServer(function(req, res){
     console.log(`${req.method} request for ${req.url}`);
 
+    if (req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+        res.writeHead(200 ,{"Content-Type":"text/html"} );
+        res.end(html);
+
+        });
 
 }).listen(4000); 
